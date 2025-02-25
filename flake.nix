46c5d65dc082f01fee756bf25d506d79503139da
Daniil Mira <daniil.mira@team-eps.de>
2025-02-25 12:49:50 +0100

    Fix: node-zopfli was marked as dev dependency Add: source code url

--- flake.nix
+++ flake.nix
@@ -1,17 +1,20 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-24.11";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
   outputs = {
     self,
     nixpkgs,
+    nixpkgs-unstable,
   }: let
     system = "x86_64-linux";
     pkgs = nixpkgs.legacyPackages.${system};
+    unstable-pkgs = nixpkgs-unstable.legacyPackages.${system};
   in {
     devShells.${system}.default = pkgs.mkShell {
-      packages = [pkgs.bun pkgs.nodejs];
+      packages = [unstable-pkgs.bun];
     };
   };
 }
